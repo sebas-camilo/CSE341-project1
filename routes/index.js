3841f585575bb5e1c48df0d81b203c007cec714b
rangerkoala <sebastiancamilopf@gmail.com>
2026-09-14 22:23:00 -0500

    corrections

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 
-router.use('/', require('./swagger'));
-
 router.get('/', (req, res) => {
     //#swagger.tags = ['Hello World']
     res.send('Hello World!');
 });
 
 router.use('/users', require('./users'));
+router.use('/api-docs', require('./swagger'));
 
 module.exports = router;
